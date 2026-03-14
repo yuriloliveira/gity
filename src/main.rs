1f12a313e97683f7482e914a1f17b76cf9cff635
yuriloliveira <yuri12194@gmail.com>
2026-03-14 09:26:19 +0100

    simple rendering of chagned paths

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 use std::env;
 use git2::Repository;
+use ratatui::{DefaultTerminal, Frame, symbols::border, text::Line, widgets::{Block, Paragraph}};
 
-fn main() -> std::io::Result<()> {
+fn main() -> color_eyre::Result<()> {
+
+    color_eyre::install()?;
+    ratatui::run(app)?;
+
+    Ok(())
+}
+
+fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
+
     let repo = match Repository::open(env::current_dir()?.display().to_string()) {
         Ok(repo) => repo,
         Err(e) => panic!("failed to open: {}", e),
@@ -12,17 +22,38 @@ fn main() -> std::io::Result<()> {
         Err(e) => panic!("failed to load statuses: {}", e)
     };
 
-    println!("Git repo {}", repo.path().display());
+    let not_ignored_statuses: Vec<String> = statuses
+        .iter()
+        .filter(|entry| !entry.status().is_ignored())
+        .filter_map(|entry| entry.path().map(|path| path.to_string()))
+        .collect();
+    
+    loop {
+        terminal.draw(build_render(&not_ignored_statuses))?;
 
-    let not_ignored_statuses_iter = statuses.iter().filter(|entry| !entry.status().is_ignored());
-
-
-    for entry in not_ignored_statuses_iter {
-        match entry.path() {
-            Some(path) => println!("{}", path),
-            None => panic!("Status is none"),
+        
+        if crossterm::event::read()?.is_key_press() {
+            break Ok(());
         }
     }
+}
 
-    Ok(())
+fn build_render(statuses: &Vec<String>) -> impl Fn(&mut Frame) {
+    move |frame| { render(statuses.clone(), frame) }
+}
+
+fn render(statuses: Vec<String>, frame: &mut Frame) {
+    let title = Line::from("Changes:");
+
+    let block = Block::bordered()
+        .title(title)
+        .border_set(border::THICK);
+
+    let lines: Vec<Line> = statuses.iter().map(|path| Line::from(vec!["[ ] ".into(), path.as_str().into()])).collect();
+
+    let paragraph = Paragraph::new(lines)
+        .left_aligned()
+        .block(block);
+
+    frame.render_widget(paragraph, frame.area());
 }
